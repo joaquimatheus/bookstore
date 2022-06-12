0123a95582410eb6c8d37d1f744c97966bcb2bda
Matheus Zakken <matheus@marcogabriel.com.br>
2022-06-11 22:05:38 -0300

    added gitignore

---
+++ sql/schema.sql
@@ -0,0 +1,13 @@
+-- DOCS 
+
+-- pk === PRIMARY KEY
+-- uq === UQ
+-- fk === FK
+-- df === DEFAULT KEY
+-- seq_nomedatabela === SEQUENCE
+
+-- Table authors
+-- publishing company
+-- table genders
+-- table translators
+-- table clients
