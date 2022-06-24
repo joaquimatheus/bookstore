59b09c8a33517de9b6256cb2bd41aa80f3002e3c
zakken <matheus@marcogabriel.com.br>
2022-06-24 17:11:27 -0300

    Function to logging the routes if is online

--- nodejs/api/routes.js
+++ nodejs/api/routes.js
@@ -2,6 +2,16 @@ module.exports = function(app) {
     const _get = app.get;
     const _post = app.post;
 
+    app.post = function(route) {
+        console.log(`Binding route: {POST} ${route}`)
+        return _post.apply(this, arguments);
+    };
+
+    app.get = function(route) {
+        console.log(`Binding route: {GET} ${route}`)
+        return _get.apply(this, arguments);
+    };
+
     require('./endpoints/products')(app);
 
     app.get = _get;
