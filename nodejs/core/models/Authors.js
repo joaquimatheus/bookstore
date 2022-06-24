4ba445e76c74b9ce8da728f10dc18f8020111ba2
zakken <matheus@marcogabriel.com.br>
2022-06-24 01:58:13 -0300

    Create two models Authors and translotors

---
+++ nodejs/core/models/Authors.js
@@ -0,0 +1,36 @@
+require('../../../dotenv');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.PSQL_DBNAME,
+    process.env.PSQL_USER,
+    process.env.PSQL_PASSWORD, {
+        host: process.env.PSQL_HOST,
+        dialect: 'postgres'
+    }
+)
+
+class Authors extends Model {}
+
+Authors.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    }
+}, {
+    sequelize,
+    modelName: 'translators',
+    timestamps: true
+});
+
+module.exports = Translators;
