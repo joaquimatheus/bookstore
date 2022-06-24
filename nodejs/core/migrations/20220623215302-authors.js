c26fced4a1eccc9b71af4c33398e6f6ea16c5fea
zakken <matheus@marcogabriel.com.br>
2022-06-24 01:57:07 -0300

    Changed the config, create two migrations authors and translators

---
+++ nodejs/core/migrations/20220623215302-authors.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+      queryInterface.createTable('authors', {
+          id: {
+              type: Sequelize.INTEGER,
+              primaryKey: true,
+              autoIncrement: true
+          },
+          name: {
+              type: Sequelize.TEXT,
+              allowNull: false
+          },
+          description: {
+              type: Sequelize.TEXT,
+              allowNull: false
+          },
+          createdAt: {
+              type: Sequelize.DATE
+          },
+          updatedAt: {
+              type: Sequelize.DATE
+          }
+      })
+  },
+
+  async down (queryInterface, Sequelize) {
+      queryInterface.dropTable('authors');
+  }
+};
