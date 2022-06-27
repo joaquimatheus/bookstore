eb594375e2bb6bf5538abe3f7da6ebfc93c1c5f4
zakken <matheus@marcogabriel.com.br>
2022-06-27 17:44:01 -0300

    Created a endpoit for products

--- nodejs/api/endpoints/products.js
+++ nodejs/api/endpoints/products.js
@@ -2,7 +2,8 @@ const bodyParser = require('body-parser');
 const Categories = require('../../core/models/Categories');
 const Translators = require('../../core/models/Translators');
 const Authors = require('../../core/models/Authors');
-const Publishers = require('../../core/models/Publishers')
+const Publishers = require('../../core/models/Publishers');
+const Products = require('../../core/models/Products');
 
 module.exports = function(app) {
     app.post('/categories', 
@@ -96,4 +97,39 @@ module.exports = function(app) {
                 });
             }
         })
+
+    app.post('/products', 
+        bodyParser.json(),
+        async function(req, res) {
+            /*
+            const { name, description, category_id, author_id, translator_id, 
+                publishers_id, pages, language, price, link, 
+                isbn13, isbn10} = req.body;
+            */
+
+            const productObj = req.body;
+            console.log(productObj);
+
+            const product = await Products.create({
+                name: productObj.name,
+                description: productObj.description,
+                category_id: productObj.category_id,
+                author_id: productObj.author_id,
+                translator_id: productObj.translator_id,
+                publisher_id: productObj.publisher_id,
+                pages: productObj.pages,
+                language: productObj.pages,
+                price: productObj.price,
+                link: productObj.link,
+                isbn13: productObj.isbn13,
+                isbn10: productObj.isbn10
+            });
+
+            if(product) {
+                res.status(200).json({
+                    msg: "Products created successfully"
+                })
+            }
+        }
+    )
 }
