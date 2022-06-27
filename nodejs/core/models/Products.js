e00ad7108b0d918ff549a6a0c2074a2dc3e5b363
zakken <matheus@marcogabriel.com.br>
2022-06-27 17:45:03 -0300

    Model products created

---
+++ nodejs/core/models/Products.js
@@ -0,0 +1,87 @@
+require('../../../dotenv');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+    process.env.PSQL_DBNAME,
+    process.env.PSQL_USER,
+    process.env.PSQL_PASSWORD, {
+        host: process.env.PSQL_HOST,
+        dialect: 'postgres'
+    }
+)
+
+class Products extends Model {}
+
+Products.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    category_id: {
+        type: DataTypes.TEXT,
+        references: {
+            model: 'categories',
+            key: 'id'
+        }
+    },
+    author_id: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'authors',
+          key: 'id'
+      }
+    },
+    translator_id: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'translators',
+          key: 'id'
+      }
+    },
+    publisher_id: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'publishers',
+          key: 'id'
+      }
+    },
+    pages: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    language: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+    },
+    link: {
+        type: Sequelize.TEXT,
+        unique: true,
+    },
+    isbn13: {
+        type: Sequelize.BIGINT,
+        unique: true,
+    },
+    isbn10: {
+        type: Sequelize.BIGINT,
+        unique: true,
+    },
+}, {
+    sequelize,
+    modelName: 'products',
+    timestamps: true
+});
+
+module.exports = Products
