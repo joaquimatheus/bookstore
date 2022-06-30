1c192b41ee3b22a594624b6142a61916dce6bf65
zakken <matheus@marcogabriel.com.br>
2022-06-30 17:55:20 -0300

    js files to send request in api to create rows

---
+++ ui/public/js/translators.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    const domqs = document.querySelector.bind(document);
+
+    domqs('form').addEventListener('submit', async (ev) => {
+        ev.preventDefault();
+
+        const formData = {
+            name: domqs('#name__input').value,
+            description: domqs('#description__input').value
+        }
+
+        await fetch('http://localhost:3000/translators', {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            mode: 'cors',
+            method: "POST",
+            body: JSON.stringify(formData)
+        })
+        .then(async(res) => console.log(res) )
+        .catch(async(res) => console.log(res))
+    })
+}
