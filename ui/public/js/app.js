b5247c6267efd8e7a2003ffa6d921ea63bbf0d06
zakken <matheus@marcogabriel.com.br>
2022-07-20 23:58:34 -0300

    api: ajaxAdapter to send request to api, config and domqs and showDataTables

---
+++ ui/public/js/app.js
@@ -0,0 +1,42 @@
+window.app = {
+    config: { apiUrl: "http://localhost:3000/api/v1" },
+    domqs: document.querySelector.bind(document),
+};
+
+window.app.ajaxAdapter = async (method, url, body, headers = {}) => {
+    headers["Content-Type"] = "application/json";
+
+    const response = await fetch(`${window.app.config.apiUrl}/${url}`, {
+        method,
+        headers,
+        body: JSON.stringify(body),
+    });
+
+    const jsonResponse = await response.json();
+
+    return jsonResponse;
+};
+
+window.app.showDataTables = async (url, el) => {
+    const element = window.app.domqs(el);
+
+    fetch(`${window.app.config.apiUrl}/${url}`)
+        .then((res) => res.json())
+        .then((obj) => {
+            const { data } = obj;
+            let output = "";
+            data.forEach((value) => {
+                output += `
+                    <tr>
+                        <td>${value.name}</td>
+                        <td>${value.description}</td>
+                        <td>${value.createdAt}</td>
+                        <td>${value.updatedAt}</td>
+                    </tr>
+                `;
+            });
+
+            element.innerHTML += output;
+        })
+        .catch((err) => console.error(err));
+};
