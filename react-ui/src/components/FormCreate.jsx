e1d7f7993fd712b4f17c34676d88679ab5291563
zakken <matheus@marcogabriel.com.br>
2022-09-20 23:15:17 -0300

    Using useState to handle inputs, and placed a new component TableProduct

--- react-ui/src/components/FormCreate.jsx
+++ react-ui/src/components/FormCreate.jsx
@@ -1,23 +1,45 @@
+import { useState } from "react";
+import TableProduct from "./TableProduct";
+
 function FormCreate(props) {
+    const [FormValues, setFormValues] = useState({});
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        console.log(`handleInput ${name, value}`)
+    }
+
     return (
         <>
-            <form className="form-product">
-                <h2>{props.title}</h2>
-                <div className="form-group">
-                    <label for="name">Name:</label>
-                    <input id="name" type="text" name="name" required />
-                </div>
-                <div className="form-group">
-                    <label for="description">Description</label>
-                    <textarea
-                        id="description"
-                        name="description"
-                        column="20"
-                        rows="10"
-                        required
-                    ></textarea>
-                </div>
-            </form>
+            <div className="content-product">
+                <form>
+                    <h2>{props.title}</h2>
+                    <div className="form-group">
+                        <label for="name">Name:</label>
+                        <input 
+                            id="name" 
+                            type="text" 
+                            name="name" 
+                            onChange={handleInputChange}
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <label for="description">Description</label>
+                        <textarea
+                            id="description"
+                            name="description"
+                            column="20"
+                            rows="10"
+                            onChange={handleInputChange}
+                            required
+                        ></textarea>
+                    </div>
+                    <div className="form-group align-center">
+                        <button>Submit</button>
+                    </div>
+                </form>
+
+                <TableProduct title={props.title} />
+            </div>
         </>
     );
 }
