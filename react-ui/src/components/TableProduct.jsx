afb80c45cf34efe04e421043d909e6df77158c33
zakken <matheus@marcogabriel.com.br>
2022-09-22 23:08:52 -0300

    Fix the poblem that it was running items.map first without the data returned from the api

--- react-ui/src/components/TableProduct.jsx
+++ react-ui/src/components/TableProduct.jsx
@@ -1,31 +1,48 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function TableProduct(props) {
     const [items, setItems] = useState(null);
 
-    useEffect(() => {
-        axios.get(`http://localhost:4000/api/v1/product-management/${props.endpoint}`)
-            .then((response) => {
-                let { data } = response.data;
-                console.log(data)
-                setItems(data);
+    const urlApi = `http://localhost:4000/api/v1/product-management/${props.endpoint}`
+
+    const getData = async ({ api }) => {
+        let apiReturn = await axios
+            .get(api)
+            .then(async (response) => {
+                return response;
             })
+            .catch((err) => {
+                console.error(err);
+            })
+        return apiReturn;
+    }
+
+    useEffect(() => {
+        const data = async api => {
+            let response = await getData({ api: api })
+            let { data } = response.data
+            setItems(data);
+        }
+
+        data(urlApi);
     }, [])
-    
+
     return (
         <table className="table-product">
             <caption>All {props.title}</caption>
             <thead>
                 <tr>
                     <th scope="col">Name</th>
-                    <th scope="col" className="th-description">Description</th>
+                    <th scope="col" className="th-description">
+                        Description
+                    </th>
                     <th scope="col">Created At</th>
                     <th scope="col">Updated At</th>
                 </tr>
             </thead>
             <tbody>
-                {items.map((item) => (
+                {items && items.map((item) => (
                 <tr>
                     <td>{item.name}</td>
                     <td>{item.description}</td>
@@ -35,7 +52,7 @@ function TableProduct(props) {
                 ))}
             </tbody>
         </table>
-    )
+    );
 }
 
 export default TableProduct;
