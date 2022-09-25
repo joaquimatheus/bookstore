7257ad68e9aaa6aaad21bfc1a04464fd75ee7e0d
zakken <matheus@marcogabriel.com.br>
2022-09-25 18:27:18 -0300

    Separate the sections in details-of-products, and to create the product -itself

--- react-ui/src/pages/ManagementProducts.jsx
+++ react-ui/src/pages/ManagementProducts.jsx
@@ -1,5 +1,5 @@
-import '../styles/management-products.css';
-import { Link } from 'react-router-dom';
+import "../styles/management-products.css";
+import { Link } from "react-router-dom";
 
 function ManagementProduct() {
     return (
@@ -7,25 +7,45 @@ function ManagementProduct() {
             <h1>Management Product</h1>
 
             <section className="product-area">
-                <div className="item-product">
-                    <h2>Categories</h2>
-                    <Link to="/management/products/categories">Create/View Categories</Link>
+                <div className="datails-product">
+                    <h3 className="heading-part">Details of Products</h3>
+                    <div className="item-product">
+                        <h2>Categories</h2>
+                        <Link to="/management/products/categories">
+                            Create/View Categories
+                        </Link>
+                    </div>
+                    <div className="item-product">
+                        <h2>Authors</h2>
+                        <Link to="/management/products/authors">
+                            Create/View Authors
+                        </Link>
+                    </div>
+                    <div className="item-product">
+                        <h2>Translators</h2>
+                        <Link to="/management/products/translators">
+                            Create/View Translators
+                        </Link>
+                    </div>
+                    <div className="item-product">
+                        <h2>Publishers</h2>
+                        <Link to="/management/products/publishers">
+                            Create/View Publishers
+                        </Link>
+                    </div>
                 </div>
-                <div className="item-product">
-                    <h2>Authors</h2>
-                    <Link to="/management/products/authors">Create/View Authors</Link>
-                </div>
-                <div className="item-product">
-                    <h2>Translators</h2>
-                    <Link to="/management/products/translators">Create/View Translators</Link>
-                </div>
-                <div className="item-product">
-                    <h2>Publishers</h2>
-                    <Link to="/management/products/publishers">Create/View Publishers</Link>
+                <div className="product-create">
+                    <h3 className="heading-part">Product itself</h3>
+                    <div className="item-product">
+                        <h2>Product</h2>
+                        <Link to="/management/products/product">
+                            Create/View Products
+                        </Link>
+                    </div>
                 </div>
             </section>
         </main>
-    )
+    );
 }
 
-export default ManagementProduct
+export default ManagementProduct;
