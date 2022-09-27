4c50e4b1d8f3223032dbfa100c01d5421fa1036d
zakken <matheus@marcogabriel.com.br>
2022-09-27 20:09:10 -0300

    Model Language

---
+++ nodejs/core/models/Languages.js
@@ -0,0 +1,31 @@
+require('../../../dotenv');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const sequelize = require('../sequelize.js');
+
+const util = require('util')
+
+class Languages extends Model {}
+
+Languages.init({
+    id: {
+        type: DataTypes.INTEGER
+        primaryKey: true
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true 
+    },
+    acronym: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    }
+}, {
+    sequelize,
+    modelName: 'languages',
+    timestamps: true
+})
+
+module.exports = Languages;
